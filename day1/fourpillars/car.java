b9ea467606c5e695138c4efb953dd37fd63dfd6c
Daud-Dev-AI <daudahmad0603@gmail.com>
2025-10-30 08:41:48 -0500

    Initial commit

---
+++ day1/fourpillars/car.java
@@ -0,0 +1,16 @@
+package workspace.day1.fourpillars;
+
+public class car {
+    String engine;
+    String make;
+    String model;
+
+    public car() {
+    }
+
+    public car(String engine, String make, String model) {
+        this.engine = engine;
+        this.make = make;
+        this.model = model;
+    }
+}
---
+++ day1/fourpillars/fourpillarsmain.java
@@ -0,0 +1,7 @@
+package workspace.day1.fourpillars;
+
+public class fourpillarsmain {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
